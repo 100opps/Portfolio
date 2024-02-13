3445f0570bb75d1791a12444941a9bafa1d56c26
100opps <c0ntact100opps@gmail.com>
2024-02-13 04:35:12 -0300

    ✨ feat: Add `types` file

---
+++ src/types.d.ts
@@ -0,0 +1,14 @@
+interface Profile {
+  network: string;
+  url: string;
+  svg: string;
+}
+
+interface Config {
+  name: string;
+  title: string;
+  label: string;
+  birthDate: string;
+  email: string;
+  profiles: Profile[];
+}
