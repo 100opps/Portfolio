95d4c525c99d1aeada2049ec7f5709bc25afc28c
100opps <c0ntact100opps@gmail.com>
2024-02-14 17:28:24 -0300

    ✨ feat:: Add `commitlint.config.js`

---
+++ commitlint.config.js
@@ -0,0 +1,23 @@
+export default {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',
+        'fix',
+        'docs',
+        'style',
+        'refactor',
+        'test',
+        'chore',
+        'perf'
+      ]
+    ],
+    'header-max-length': [2, 'always', 100],
+    'scope-case': [2, 'always', 'lower-case'],
+    'subject-case': [2, 'always', 'lower-case'],
+    'subject-full-stop': [2, 'never', '.'],
+  },
+};
