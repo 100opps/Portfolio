ef298ed3eb93dd8c8bc9052787bd0d140425d6dd
100opps <c0ntact100opps@gmail.com>
2024-02-14 17:35:34 -0300

    💄 style: Format code with `ESLint`

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,20 +1,7 @@
 export default {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'type-enum': [
-      2,
-      'always',
-      [
-        'feat',
-        'fix',
-        'docs',
-        'style',
-        'refactor',
-        'test',
-        'chore',
-        'perf'
-      ]
-    ],
+    'type-enum': [2, 'always', ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'chore', 'perf']],
     'header-max-length': [2, 'always', 100],
     'scope-case': [2, 'always', 'lower-case'],
     'subject-case': [2, 'always', 'lower-case'],
